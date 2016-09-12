86d1a04c2ea5e3e054eb5f451d3fa7a46cbb3f91
Ido Winter <ido.winter@gmail.com>
2016-09-12 16:39:34 +0300

    Added the main files of the bare bones tutorial.

---
+++ build.sh
@@ -0,0 +1,12 @@
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+if grub-file --is-x86-multiboot myos.bin; then
+  echo multiboot confirmed
+else
+  echo the file is not multiboot
+fi
+mkdir -p isodir/boot/grub
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
+
