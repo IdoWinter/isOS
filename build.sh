fb69e5334413872cbd5b3890d994675aade82087
Ido Winter <ido.winter@gmail.com>
2016-09-26 11:37:33 +0300

    Organized files.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -c kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
 if grub-file --is-x86-multiboot myos.bin; then
   echo multiboot confirmed
 else
@@ -7,6 +7,7 @@ else
 fi
 mkdir -p isodir/boot/grub
 cp myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
+mkdir -p bin
+cp myos.bin bin/myos.bin
+cp build/grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir
-
